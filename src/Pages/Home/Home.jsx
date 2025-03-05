437516768ddf10f38842215a8c18cf22544da75b
shafriki <shafriki.cse@gmail.com>
2025-03-05 13:50:49 +0600

    added about section in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BannerCompo from '../../components/Banner/BannerCompo';
 import Latest from '../../components/Latest Events/Latest';
+import AboutUs from '../../components/AboutUs/AboutUs';
 
 const Home = () => {
     return (
         <div>
             <BannerCompo></BannerCompo>
             <Latest></Latest>
+            <AboutUs></AboutUs>
         </div>
     );
 };
---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+import { Typewriter } from 'react-simple-typewriter'; 
+
+const AboutUs = () => {
+  return (
+    <div className="bg-amber-50">
+        <div className="flex flex-col lg:flex-row justify-between gap-10 max-w-screen-xl md:mx-auto items-center p-6  rounded-xl lg:p-12  mx-3 my-32">
+
+{/* img content */}
+<div className="w-full lg:w-2/5">
+  <Fade cascade damping={0.1}>
+    <h2 className="text-base md:text-2xl font-bold text-[#228B22] mb-6">
+      Let’s Talk About HandsOn Team Journey
+    </h2>
+
+    <div className="grid grid-cols-2 gap-1 md:gap-4">
+      <img src="https://i.ibb.co.com/hg1XtZ0/a-1.webp" className="w-full h-32 md:h-48 object-cover rounded-lg"/>
+      
+      <img src="https://i.ibb.co.com/556qsrW/a-2.webp" className="w-full h-32 md:h-48 object-cover rounded-lg" />
+
+      <img src="https://i.ibb.co.com/D45pbqm/a4.webp" className="w-full h-32 md:h-48 object-cover rounded-lg" />
+      
+      <img src="https://i.ibb.co.com/gDkFNcn/a-4.webp" className="w-full h-32 md:h-48 object-cover rounded-lg" />
+    </div>
+  </Fade>
+</div>
+
+{/* text content */}
+<div className="w-full lg:w-3/5 text-left">
+  <Fade cascade damping={0.1}>
+    <p className="text-sm text-[#228B22] mb-4">Join Us Today!</p>
+
+    {/* react type writer text */}
+    <h1 className="text-2xl md:text-4xl font-bold text-gray-800 mb-1">
+      <Typewriter
+        words={[
+          "About HandsOn Team",
+          "About Our Mission",
+          "About Our Vission"
+        ]}
+        loop={0} 
+        cursor
+        cursorStyle="_"
+        typeSpeed={100}
+        deleteSpeed={50}
+        delaySpeed={1500}/>
+    </h1>
+
+    <div className="flex items-start">
+      <div>
+        <p className="text-xs text-justify text-gray-600 mt-2 md:text-base">
+            We are a community-driven initiative dedicated to connecting medical volunteers with those in need. Our mission is to provide essential healthcare support, emergency aid, and wellness programs to underserved communities. Through compassion, expertise, and teamwork, we strive to make a meaningful impact and create a healthier, more caring world.
+        </p>
+      </div>
+    </div>
+    
+  </Fade>
+</div>
+</div>
+    </div>
+  );
+};
+
+export default AboutUs;
